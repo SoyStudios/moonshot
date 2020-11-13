d3faf9ef9442aca18ced556f01bcac244d070609
Seong-Min Kang <mail@seong.de>
2020-11-13 08:26:51 +0100

    lexer; read contiguous idents

--- parser.go
+++ parser.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"io"
+	"strings"
 )
 
 var eof = rune(0)
@@ -14,6 +15,10 @@ func isWhitespace(ch rune) bool {
 
 func isLetter(ch rune) bool {
 	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
+}
+
+func isDigit(ch rune) bool {
+	return ch >= '0' && ch <= '9'
 }
 
 type Scanner struct {
@@ -43,7 +48,15 @@ func (s *Scanner) Scan() (tok Token, lit string) {
 		s.unread()
 		return s.scanWhitespace()
 	} else if isLetter(ch) {
+		s.unread()
+		return s.scanIdent()
+	}
 
+	switch ch {
+	case eof:
+		return EOF, ""
+	default:
+		return ILLEGAL, string(ch)
 	}
 }
 
@@ -66,5 +79,24 @@ func (s *Scanner) scanWhitespace() (tok Token, lit string) {
 }
 
 func (s *Scanner) scanIdent() (tok Token, lit string) {
+	var buf bytes.Buffer
+	buf.WriteRune(s.read())
 
+	for {
+		if ch := s.read(); ch == eof {
+			break
+		} else if !isLetter(ch) && !isDigit(ch) {
+			s.unread()
+			break
+		} else {
+			buf.WriteRune(ch)
+		}
+	}
+
+	switch strings.ToUpper(buf.String()) {
+	case "RDX":
+		return RDX, buf.String()
+	default:
+		return CONST, buf.String()
+	}
 }
